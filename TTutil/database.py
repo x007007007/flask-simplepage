10c56410166d3f65a7b3903271f94ebf11aa979a
xxc <x007007007@126.com>
2014-03-31 23:36:52 +0800

    数据库继承链测试

--- TTutil/database.py
+++ TTutil/database.py
@@ -4,9 +4,10 @@
 
 import time
 from sqlalchemy import create_engine
-from sqlalchemy import Column, String, TIMESTAMP, Boolean
+from sqlalchemy import Column, String, Boolean ,Integer
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import uuid
 
 engine = create_engine('sqlite:////tmp/test.db', convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
@@ -17,21 +18,55 @@ BaseDB.query = db_session.query_property()
 
 
 class BaseObjectDB(BaseDB):
+    __abstract__=True
     uuid = Column(String(128), primary_key=True)
     alias = Column(String(128))
-    create_at = Column(TIMESTAMP())
-    modify_at = Column(TIMESTAMP())
+    create_at = Column(Integer())
+    modify_at = Column(Integer())
     
-    def __init__(self,*args,**kwargs):
-        self.uuid="..."
+    def __init__(self,uuid_prefix=None,*args,**kwargs):
+        if uuid_prefix is None:
+            self.uuid="%s_%s%s"%(uuid_prefix,
+                                 str(uuid.uuid1())[:8],
+                                 str(uuid.uuid1())[:8])
+        else:
+            self.uuid="%s_%s%s"%(self.__tablename__[5:],
+                                 str(uuid.uuid1())[:8],
+                                 str(uuid.uuid1())[:8])
         self.create_at=time.time()
+    
+    def __repr__(self):
+        return "<%s> %s:%s"%(self.__tablename__,self.uuid,self.alias)
 
 
 class BaseConfigDB(BaseObjectDB):
+    __abstract__=True
     enable= Column(Boolean())
+    
+    def __repr__(self):
+        return "<%s> %s %s:%s"%(self.__tablename__,
+                                str(self.enable),
+                                self.uuid,self.alias)
+
+
+class TestConfigData(BaseConfigDB):
+    __tablename__="test"
+    data=Column(String())
+    def __init__(self,data="helloworld",*args,**kwargs):
+        super(BaseConfigDB,self).__init__(*args,**kwargs)
+        self.data=data
+    
+data=TestConfigData("xxc")
+
+
 
 
 def init_db():
 
-
     BaseDB.metadata.create_all(bind=engine)
+    
+    
+#init_db()
+data=TestConfigData("xxc")
+db_session.add(data)
+db_session.commit()
